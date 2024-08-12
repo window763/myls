08998ae8306d83dda65a4816a822c016187ebb81
window763 <window763@DESKTOP-7F28A89>
2024-08-12 19:54:12 +0530

    Added commas to the end of each final argparse argument. Also adding this commit message to resolve Issue 1 on GitHub. Fixes: #1

--- pyls.py
+++ pyls.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(
         prog="pyls",
         description="Outputs a list of files in a given directory (or present working directory if no input given)",
-        epilog="That's all folks."
+        epilog="That's all folks.",
         )
 
 parser.add_argument(
@@ -11,21 +11,21 @@ parser.add_argument(
         help="Name of directory to list the contents of",
         action="store",
         nargs="?",
-        default="."
+        default=".",
         )
 
 parser.add_argument(
         "-l",
         "--long-format",
         help="Outputs list of files + metadata in the following format: Last Modified Date (YYYY-MM-DD HH:MM:SS), Size of Entry, Filename).",
-        action="store-true"
+        action="store-true",
         )
 
 parser.add_argument(
         "-F",
         "--filetype",
         help="Adds an extra character to the end of the printed filename that indicates whether a file is a directory (/) or an executable (*).",
-        action="store-true"
+        action="store-true",
         )
 
 args = parser.parse_args()
