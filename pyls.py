617ba68ba1fb5bf907fbfa64cfd7dab9200be51c
window763 <window763@DESKTOP-7F28A89>
2024-08-12 19:44:08 +0530

    Adding pyls.py to repo. Includes implementation of agruments and flags using the argparse library in python.

---
+++ pyls.py
@@ -0,0 +1,36 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+        prog="pyls",
+        description="Outputs a list of files in a given directory (or present working directory if no input given)",
+        epilog="That's all folks."
+        )
+
+parser.add_argument(
+        "directory_name",
+        help="Name of directory to list the contents of",
+        action="store",
+        nargs="?",
+        default="."
+        )
+
+parser.add_argument(
+        "-l",
+        "--long-format",
+        help="Outputs list of files + metadata in the following format: Last Modified Date (YYYY-MM-DD HH:MM:SS), Size of Entry, Filename).",
+        action="store-true"
+        )
+
+parser.add_argument(
+        "-F",
+        "--filetype",
+        help="Adds an extra character to the end of the printed filename that indicates whether a file is a directory (/) or an executable (*).",
+        action="store-true"
+        )
+
+args = parser.parse_args()
+
+"""
+Three arguments are checked for in the input gathering step,
+with the flag arguments being stored as bools.
+"""
